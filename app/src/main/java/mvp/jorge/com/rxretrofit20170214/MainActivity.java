9113afb36ed1ddbaf9d9ef0b23a7f3df3dfd4c12
zhangjun <zhangjun6@cofco.com>
2017-02-14 10:59:29 +0800

    Initial commit

---
+++ app/src/main/java/mvp/jorge/com/rxretrofit20170214/MainActivity.java
@@ -0,0 +1,13 @@
+package mvp.jorge.com.rxretrofit20170214;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/mvp/jorge/com/rxretrofit20170214/NetWork.java
@@ -0,0 +1,9 @@
+package mvp.jorge.com.rxretrofit20170214;
+
+/**
+ * @author zhangjun on 2017/2/14.
+ */
+
+public class NetWork {
+
+}
